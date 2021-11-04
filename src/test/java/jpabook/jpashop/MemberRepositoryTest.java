6b0b5b96fc9ed45f7e93fcde31891c454c86459e
danaRyu <dana40426@gmail.com>
2021-11-04 22:05:13 +0900

    DataBase Install & JPA Test

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    // Transactional이 Test case에 있으면, Test가 끝나고 Rollback한다. Rolled back transaction for test
+    @Rollback(false) // 이 annotaion을 추가하면 Rollback을 하지 않고, Commit한다.
+    @Transactional
+    public void testMember() throws Exception {
+
+        //given
+        Member member = new Member();
+        member.setUsername("danadot");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+        // 영속성 Context에서 식별자가 같으면, 같은 객체 (1차 캐쉬)
+        System.out.println("findMember == member:  " + (findMember == member));
+
+    }
+}
